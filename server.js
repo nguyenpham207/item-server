8279826edb9ce60610fb26a9e2a2f0e52692d48e
nguyenpham207 <seanphamgd@gmail.com>
2022-10-05 00:27:53 -0700

    setting up

---
+++ server.js
@@ -0,0 +1,12 @@
+/////////////////////////////////////////////
+// Import Our Dependencies
+/////////////////////////////////////////////
+require("dotenv").config() // Load ENV Variables
+const express = require("express") // import express
+
+const morgan = require("morgan") // import morgan
+const mongoose = require("mongoose") // import mongoose
+const path = require("path") // import path module
+
+
+const app = express()
